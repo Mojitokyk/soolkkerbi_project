6a23d92ec2f3cc7c704fc3f72faec938a9b92447
gyeongeunpark <142005747+gyeongeunpark@users.noreply.github.com>
2023-09-27 14:12:49 +0900

    app.js에 route추가

--- soolkkerbi_project_front/src/App.js
+++ soolkkerbi_project_front/src/App.js
@@ -1,8 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
+import MypageMain from "./component/mypage/MypageMain";
 
 function App() {
-  return <div className="wrap"></div>;
+  return (
+    <div className="wrap">
+      <div className="content">
+        <Routes>
+          <Route path="/mypage/*" element={<MypageMain />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
