f07dc77593d3b3dbe2bf41abc37e71ddce2c3896
DonghyoSon <143981349+DonghyoSon@users.noreply.github.com>
2023-09-27 19:22:48 +0900

    swiper 수정

--- soolkkerbi_project_front/src/component/mainpage/Main.js
+++ soolkkerbi_project_front/src/component/mainpage/Main.js
@@ -1,22 +1,21 @@
 import { useState } from "react";
-import SwiperCore, { Autoplay } from "swiper";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
 import "./main.css";
 
 const Main = () => {
-  const [swiper, setSwiper] = useState(null);
-  const swiperParams = {
-    navigation: true,
-    onSwiper: setSwiper,
-    autoplay: { delay: 3000, disableOnInteraction: false },
-    loop: true,
-  };
-  SwiperCore.use([Autoplay]);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
   return (
     <div className="main-slide">
-      <Swiper {...swiperParams} ref={setSwiper}>
+      <Swiper
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        loop
+      >
         <SwiperSlide>
           <img src="/image/main_img/main1.png"></img>
         </SwiperSlide>
