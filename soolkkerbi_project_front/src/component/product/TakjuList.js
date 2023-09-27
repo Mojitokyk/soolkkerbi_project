a68d006887eaa8a1c70379e3a52c6ad0afef656a
khsgsong <142006448+khsgsong@users.noreply.github.com>
2023-09-27 17:12:22 +0900

    productList.css 추가

--- soolkkerbi_project_front/src/component/product/TakjuList.js
+++ soolkkerbi_project_front/src/component/product/TakjuList.js
@@ -1,29 +1,118 @@
 import "./productList.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
-const TakjuList = (props) => {
-  const isLogin = props.isLogin;
-  const [takjuList, setTakjuList] = useState([]);
-  const [reqPage, setReqPage] = useState(1);
-  const [pageInfo, setPageInfo] = useState({});
-  useEffect(() => {
-    axios
-      .get("/product/takju/list" + reqPage)
-      .then((res) => {
-        setTakjuList(res.data.takjuList);
-        setPageInfo(res.data.pi);
-      })
-      .catch((res) => {
-        console.log(res.response.status);
-      });
-  }, [reqPage]);
+const TakjuList = () => {
   return (
-    <div>
-      <div className="product-list-wrap"></div>
+    <div className="product-all-wrap">
+      <div className="product-title">탁주</div>
+      <div className="product-list-wrap">
+        <ProductItem />
+      </div>
       <div className="product-page"></div>
     </div>
   );
 };
 
+const ProductItem = () => {
+  return (
+    <>
+      <div className="product-item">
+        <div className="product-item-img">
+          <img src="/image/product_img/막쿠르트.jpg" />
+        </div>
+        <div className="product-item-info">
+          <div className="product-item-name">양(陽) 막걸리</div>
+          <div className="product-item-price">22000원</div>
+          <div className="product-item-more">
+            <div className="product-item-star">
+              <span className="material-icons">star</span>
+              <span className="star-rate">4</span>
+            </div>
+            <div className="product-item-cart">
+              <span className="material-icons">shopping_cart</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="product-item">
+        <div className="product-item-img">
+          <img src="/image/product_img/술취한원숭이.jpg" />
+        </div>
+        <div className="product-item-info">
+          <div className="product-item-name">양(陽) 막걸리</div>
+          <div className="product-item-price">22000원</div>
+          <div className="product-item-more">
+            <div className="product-item-star">
+              <span className="material-icons">star</span>
+              <span className="star-rate">4</span>
+            </div>
+            <div className="product-item-cart">
+              <span className="material-icons">shopping_cart</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="product-item">
+        <div className="product-item-img">
+          <img src="/image/product_img/붉은원숭이.jpg" />
+        </div>
+        <div className="product-item-info">
+          <div className="product-item-name">양(陽) 막걸리</div>
+          <div className="product-item-price">22000원</div>
+          <div className="product-item-more">
+            <div className="product-item-star">
+              <span className="material-icons">star</span>
+              <span className="star-rate">4</span>
+            </div>
+            <div className="product-item-cart">
+              <span className="material-icons">shopping_cart</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="product-item">
+        <div className="product-item-img">
+          <img src="/image/product_img/복순도가.jpg" />
+        </div>
+        <div className="product-item-info">
+          <div className="product-item-name">양(陽) 막걸리</div>
+          <div className="product-item-price">22000원</div>
+          <div className="product-item-more">
+            <div className="product-item-star">
+              <span className="material-icons">star</span>
+              <span className="star-rate">4</span>
+            </div>
+            <div className="product-item-cart">
+              <span className="material-icons">shopping_cart</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="product-item">
+        <div className="product-item-img">
+          <img src="/image/product_img/막걸리.jpg" />
+        </div>
+        <div className="product-item-info">
+          <div className="product-item-name">
+            양(陽) 막걸리
+            야야야야야어리ㅏ어ㅣ츹,ㅡ치더랴더시ㅏ얼피아아ㅣㅇ리ㅓㅏㅇ리ㅏ
+          </div>
+          <div className="product-item-price">22000원</div>
+          <div className="product-item-more">
+            <div className="product-item-star">
+              <span className="material-icons">star</span>
+              <span className="star-rate">4</span>
+            </div>
+            <div className="product-item-cart">
+              <span className="material-icons">shopping_cart</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default TakjuList;
