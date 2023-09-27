de2d08f4c418433a8bb134419d738baa55810eef
khsgsong <142006448+khsgsong@users.noreply.github.com>
2023-09-27 12:12:38 +0900

    product폴더 내 파일 컴포넌트 생성

---
+++ soolkkerbi_project_front/src/component/product/Cart.js
@@ -0,0 +1,11 @@
+import "./cart.css";
+
+const Cart = () => {
+  return (
+    <div>
+      <div>장바구니</div>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ soolkkerbi_project_front/src/component/product/Order.js
@@ -0,0 +1,11 @@
+import "./order.css";
+
+const Order = () => {
+  return (
+    <div>
+      <div>주문하기</div>
+    </div>
+  );
+};
+
+export default Order;
