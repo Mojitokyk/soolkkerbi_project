d569172612c2e649b2a678084522271b5e1a3dd7
gyeongeunpark <142005747+gyeongeunpark@users.noreply.github.com>
2023-09-27 17:48:03 +0900

    화면 셋팅중

--- soolkkerbi_project_front/src/component/mypage/MyReview.js
+++ soolkkerbi_project_front/src/component/mypage/MyReview.js
@@ -3,6 +3,19 @@ const MyReview = () => {
   return (
     <div className="mypage-content-wrap">
       <div className="mypage-content-title">작성 후기</div>
+      <div className="my-reviewList-tbl">
+        <table>
+          <thead>
+            <tr>
+              <td width={"10%"}>리뷰번호</td>
+              <td width={"50%"}>리뷰제목</td>
+              <td width={"25%"}>작성날짜</td>
+              <td width={"15%"}>조회수</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
     </div>
   );
 };
