35b870b61428e25df94ffb72dda5702d71692fa6
Mojitokyk <dbsrms90@naver.com>
2023-09-27 20:03:17 +0900

    페이지네이션 생성

--- soolkkerbi_project_front/src/component/admin/ReadAllMember.js
+++ soolkkerbi_project_front/src/component/admin/ReadAllMember.js
@@ -1,6 +1,23 @@
 import "./readAllMember.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ReadAllMember = () => {
+  const [memberList, setMemberList] = useState([]);
+  const [pageInfo, setPageInfo] = useState({});
+  const [reqPage, setReqPage] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get("/member/list/" + reqPage)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((res) => {
+        console.log(res.response.status);
+      });
+  }, [reqPage]);
+
   return (
     <div className="readAllMember-wrap">
       <div className="readAllMember-title">전체회원목록</div>
