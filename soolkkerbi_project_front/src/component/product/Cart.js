b955917af955c2cb8ba2f92775261c3849991a63
khsgsong <142006448+khsgsong@users.noreply.github.com>
2023-10-04 14:03:33 +0900

    장바구니.css 완료

--- soolkkerbi_project_front/src/component/product/Cart.js
+++ soolkkerbi_project_front/src/component/product/Cart.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Button1 } from "../util/Buttons";
 import "./cart.css";
 
@@ -23,62 +24,72 @@ const Cart = () => {
             </tr>
           </thead>
           <tbody>
+            <CartProduct />
+            <CartProduct />
+          </tbody>
+        </table>
+      </div>
+      <div className="delete-btn">
+        <Button1 text="전체상품 삭제" />
+        <Button1 text="선택상품 삭제" />
+      </div>
+      <div className="cart-price-tbl">
+        <table>
+          <thead>
             <tr>
               <td>
-                <input type="checkbox" />
-              </td>
-              <td className="info-td">
-                <div className="product-img">
-                  <img src="/image/product_img/takju1.jpg" />
-                </div>
-                <div className="product-name">양(陽) 막걸리</div>
-                <span className="material-icons">close</span>
-              </td>
-              <td>
-                <div className="product-quantity">
-                  <span className="material-icons">add</span>1
-                  <span className="material-icons">remove</span>
-                </div>
-              </td>
-              <td>
-                33000원
-                <div className="cart-btn">
-                  <Button1 text="바로구매" />
-                </div>
+                <span>총 주문 상품 </span>
+                <span>1개</span>
               </td>
             </tr>
+          </thead>
+          <tbody>
             <tr>
               <td>
-                <input type="checkbox" />
-              </td>
-              <td className="info-td">
-                <div className="product-img">
-                  <img src="/image/product_img/takju1.jpg" />
-                </div>
-                <div className="product-name">양(陽) 막걸리</div>
-                <span className="material-icons">close</span>
-              </td>
-              <td>
-                <div className="product-quantity">
-                  <span className="material-icons">add</span>1
-                  <span className="material-icons">remove</span>
-                </div>
-              </td>
-              <td>
-                33000원
-                <div className="cart-btn">
-                  <Button1 text="바로구매" />
-                </div>
+                <p>총 주문금액</p>
+                <p className="cart-price">66000원</p>
               </td>
             </tr>
           </tbody>
         </table>
-      </div>
-      <div className="cart-btn">
-        <Button1 text="전체상품 삭제" />
-        <Button1 text="선택상품 삭제" />
+        <div className="pay-btn">
+          <Button1 text="주문하기" />
+        </div>
+        <div className="more-shopping">
+          <Link to="/">계속 쇼핑하기</Link>
+        </div>
       </div>
     </div>
+  );
+};
+
+const CartProduct = () => {
+  return (
+    <tr>
+      <td>
+        <input type="checkbox" />
+      </td>
+      <td className="info-td">
+        <div className="product-img">
+          <img src="/image/product_img/takju1.jpg" />
+        </div>
+        <div className="product-name">양(陽) 막걸리</div>
+        <span className="material-icons">close</span>
+      </td>
+      <td>
+        <div className="product-quantity-wrap">
+          <span className="material-icons">add</span>
+          <span className="product-quantity">1</span>
+          <span className="material-icons">remove</span>
+        </div>
+      </td>
+      <td>
+        <span className="product-price">33000원</span>
+        <div className="cart-btn">
+          <Button1 text="바로 구매" />
+        </div>
+      </td>
+    </tr>
   );
 };
 
