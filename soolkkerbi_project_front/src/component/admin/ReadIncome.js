436160a18586084394095709b374187add4ed91c
Mojitokyk <dbsrms90@naver.com>
2023-10-04 18:11:54 +0900

    매출현황조회 생성

---
+++ soolkkerbi_project_front/src/component/admin/ReadIncome.js
@@ -0,0 +1,9 @@
+const ReadIncome = () => {
+  return (
+    <div className="admin-content-wrap">
+      <div className="admin-content-title">매출현황조회</div>
+    </div>
+  );
+};
+
+export default ReadIncome;
