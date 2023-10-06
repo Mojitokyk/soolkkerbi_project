06312932eb5207203a1432eba51c4ae16783adec
khsgsong <142006448+khsgsong@users.noreply.github.com>
2023-10-06 18:04:30 +0900

    결제하기_CSS 추가

--- soolkkerbi_project_front/src/component/product/Pay.js
+++ soolkkerbi_project_front/src/component/product/Pay.js
@@ -11,23 +11,25 @@ const Pay = (props) => {
       <div className="pay-wrap">
         <div className="pay-title">결제하기</div>
         <div className="left-wrap">
-          <div className="pay-product-info">
+          <div className="pay-product-info-wrap">
             <h3>주문 상품 정보</h3>
-            <div>
+            <div className="pay-product-info">
               <div className="pay-product-img">
                 <img src="/image/product_img/takju1.jpg" />
               </div>
-              <div className="pay-product-info">
-                <span>양(陽) 막걸리</span>
-                <span>1개</span>
-                <span>33000원</span>
+              <div className="pay-product-info-detail">
+                <div>양(陽) 막걸리</div>
+                <div>1개</div>
+                <div>33000원</div>
               </div>
             </div>
           </div>
           <div className="pay-member-info"></div>
           <div className="pay-pickup-info"></div>
         </div>
-        <div className="right-wrap"></div>
+        <div className="right-wrap">
+          <div className="pay-pickup1-info"></div>
+        </div>
       </div>
     </div>
   );
