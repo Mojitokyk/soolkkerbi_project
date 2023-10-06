dd93b0a72a7f38b2983b8332846f55ceafa18aa4
DonghyoSon <143981349+DonghyoSon@users.noreply.github.com>
2023-10-06 21:02:46 +0900

    문의 답변 수정 진행 중

--- soolkkerbi_project_front/src/component/mypage/MyQnaAnswer.js
+++ soolkkerbi_project_front/src/component/mypage/MyQnaAnswer.js
@@ -48,12 +48,8 @@ const RegistAnswer = (props) => {
           console.log(res.data);
           setAnswerContent("");
 
-          // const insertAnswer = {
-          //   answerQnaNo: answerQnaNo,
-          //   answerContent: answerContent,
-          // };
           const newArr = [...answerList];
-          newArr.push(res.data); //newArr.push(insertAnswer);
+          newArr.push(res.data);
           setAnswerList(newArr);
         })
         .catch((res) => {
@@ -104,6 +100,7 @@ const PrintAnswer = (props) => {
   const answerList = props.answerList;
   const setAnswerList = props.setAnswerList;
   const [answerContent, setAnswerContent] = useState("");
+  const [modifyFrm, setModifyFrm] = useState(false);
 
   useEffect(() => {
     axios
@@ -138,19 +135,115 @@ const PrintAnswer = (props) => {
   };
 
   //답변 수정 함수
+  /*
   const modifyAnswer = (answerNo, index) => {
     console.log("답변 수정 함수 클릭");
     console.log(answerNo);
     console.log(index);
 
-    axios
-      .get("/qna/modifyAnswer/" + answerNo)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((res) => {
-        console.log(res.response.status);
-      });
+    const modifyAnswerComplete = () => {
+      // if (qnaComment !== "" && memberLevel === 1) {
+      if (answerContent !== "") {
+        const form = new FormData();
+        form.append("answerContent", answerContent);
+        form.append("answerNo", answerNo);
+        // form.append("memberId", memberId); - memberId -> qnaMemberNo 임시로 DB의 관리자 번호(62)를 넣음
+        axios
+          .get("/qna/modifyAnswer/" + answerNo)
+          .then((res) => {
+            console.log(res.data);
+            setAnswerContent("");
+
+            const newArr = [...answerList];
+            newArr.push(res.data);
+            setAnswerList(newArr);
+          })
+          .catch((res) => {
+            console.log(res.response.status);
+          });
+      } else {
+        Swal.fire("입력값을 확인하세요.");
+      }
+    };
+    //textarea의 변화를 감지한 후, 값을 answerContent에 set하는 함수
+    const changeContent = (e) => {
+      const inputValue = e.currentTarget.value;
+      setAnswerContent(inputValue);
+    };
+    //Enter로 '등록'을 수행하는 함수
+    const enterCheck = (e) => {
+      if (e.keyCode === 13) {
+        modifyAnswerComplete();
+      }
+    };
+
+    return (
+      <div className="write-answer-frm">
+        <ul>
+          <li>
+            <span>관리자</span>
+          </li>
+          <li>
+            <textarea
+              className="answer-textarea"
+              value={answerContent || ""}
+              onChange={changeContent}
+              onKeyUp={enterCheck}
+            ></textarea>
+          </li>
+          <li>
+            <Button1 text="수정완료" clickEvent={modifyAnswerComplete} />
+          </li>
+        </ul>
+      </div>
+    );
+  };
+*/
+
+  const modifyAnswerFrm = (answerNo, index) => {
+    console.log("답변 수정양식 함수 클릭");
+    console.log(answerNo);
+    console.log(index);
+
+    setModifyFrm(true);
+  };
+  const modifyAnswer = (props) => {
+    const answerNo = props.answerNo;
+
+    // if (qnaComment !== "" && memberLevel === 1) {
+    if (answerContent !== "") {
+      const form = new FormData();
+      form.append("answerContent", answerContent);
+      form.append("answerNo", answerNo);
+      // form.append("memberId", memberId); - memberId -> qnaMemberNo 임시로 DB의 관리자 번호(62)를 넣음
+      axios
+        .get("/qna/modifyAnswer/" + answerNo)
+        .then((res) => {
+          console.log(res.data);
+          setAnswerContent("");
+
+          const newArr = [...answerList];
+          newArr.push(res.data);
+          setAnswerList(newArr);
+        })
+        .catch((res) => {
+          console.log(res.response.status);
+        });
+    } else {
+      Swal.fire("입력값을 확인하세요.");
+    }
+  };
+
+  //textarea의 변화를 감지한 후, 값을 answerContent에 set하는 함수
+  const changeContent = (e) => {
+    const inputValue = e.currentTarget.value;
+    setAnswerContent(inputValue);
+  };
+  //Enter로 '등록'을 수행하는 함수
+  const enterCheck = (e) => {
+    if (e.keyCode === 13) {
+      modifyAnswer();
+    }
   };
 
   return (
@@ -158,34 +251,61 @@ const PrintAnswer = (props) => {
       {answerList.map((answer, index) => {
         return (
           <div className="print-qnaComment-wrap" key={index}>
-            <ul>
-              <li>
-                <span>관리자</span>
-              </li>
-              <li>
-                <p className="qnaComment-content">
-                  {answer.answerNo}
-                  {answer.answerDate}
-                  {answer.answerContent}
-                </p>
-                <p className="qnaComment-link">
-                  <span
-                    onClick={() => {
-                      modifyAnswer(answer.answerNo, index);
-                    }}
-                  >
-                    수정
-                  </span>
-                  <span
-                    onClick={() => {
-                      deleteAnswer(answer.answerNo, index);
-                    }}
-                  >
-                    삭제
-                  </span>
-                </p>
-              </li>
-            </ul>
+            {modifyFrm === false ? (
+              <ul>
+                <li>
+                  <span>관리자</span>
+                </li>
+                <li>
+                  <p className="qnaComment-content">
+                    {answer.answerNo}
+                    {answer.answerDate}
+                    {answer.answerContent}
+                  </p>
+                  <p className="qnaComment-link">
+                    <span
+                      onClick={() => {
+                        modifyAnswerFrm(answer.answerNo, index);
+                      }}
+                    >
+                      수정
+                    </span>
+                    <span
+                      onClick={() => {
+                        deleteAnswer(answer.answerNo, index);
+                      }}
+                    >
+                      삭제
+                    </span>
+                  </p>
+                </li>
+              </ul>
+            ) : (
+              <div className="write-answer-frm">
+                <ul>
+                  <li>
+                    <span>관리자</span>
+                  </li>
+                  <li>
+                    <textarea
+                      className="answer-textarea"
+                      value={answerContent || ""}
+                      onChange={changeContent}
+                      onKeyUp={enterCheck}
+                      placeholder={answer.answerContent}
+                    ></textarea>
+                    <input type="text" value={answer.answerNo} />
+                  </li>
+                  <li>
+                    <Button1
+                      text="수정완료"
+                      clickEvent={modifyAnswer}
+                      answerNo={answer.answerNo}
+                    />
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         );
       })}
