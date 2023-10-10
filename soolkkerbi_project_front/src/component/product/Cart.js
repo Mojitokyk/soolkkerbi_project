8a208c23ed31fe9adc32acde99a991e66b41a82b
khsgsong <142006448+khsgsong@users.noreply.github.com>
2023-10-10 10:46:03 +0900

    결제하기_완료(결제 버튼 제외)

--- soolkkerbi_project_front/src/component/product/Cart.js
+++ soolkkerbi_project_front/src/component/product/Cart.js
@@ -13,6 +13,7 @@ const Cart = (props) => {
   //totalCount.productNumber : 장바구니 품목 건수
   //totalCount.totalPrice : 장바구니 총 합계 금액
   const [totalCount, setTotalCount] = useState({});
+  const [member, setMember] = useState({});
   //체크 박스 체크 시 cartNo 넣어줌
   const [checkList, setCheckList] = useState([]);
   //장바구니 조회 및 장바구니 내 합계 금액, 품목 건수 조회
@@ -26,6 +27,7 @@ const Cart = (props) => {
       .then((res) => {
         setCartList(res.data.cartList);
         setTotalCount(res.data.totalCount);
+        setMember(res.data.member);
       })
       .catch((res) => {
         console.log(res.response.status);
@@ -168,6 +170,7 @@ const Cart = (props) => {
                   deleteOneCart={deleteOneCart}
                   plusCart={plusCart}
                   removeCart={removeCart}
+                  member={member}
                 />
               );
             })}
@@ -177,7 +180,7 @@ const Cart = (props) => {
       <div className="delete-btn">
         <Button1 text="선택상품 삭제" clickEvent={deleteCart} />
       </div>
-      <CartPrice totalCount={totalCount} cartList={cartList} />
+      <CartPrice totalCount={totalCount} cartList={cartList} member={member} />
     </div>
   );
 };
@@ -189,6 +192,7 @@ const CartProduct = (props) => {
   const deleteOneCart = props.deleteOneCart;
   const plusCart = props.plusCart;
   const removeCart = props.removeCart;
+  const member = props.member;
   const navigate = useNavigate();
   const productView = () => {
     navigate("/product/view", { state: { productNo: cart.cartProductNo } });
@@ -200,7 +204,7 @@ const CartProduct = (props) => {
   //바로 구매 버튼 클릭 시 결제페이지 이동
   const partialPay = () => {
     navigate("/product/pay", {
-      state: { cart: cart },
+      state: { cart: cart, member: member },
     });
   };
   return (
@@ -266,10 +270,15 @@ const CartProduct = (props) => {
 const CartPrice = (props) => {
   const totalCount = props.totalCount;
   const cartList = props.cartList;
+  const member = props.member;
   const navigate = useNavigate();
   const allPay = () => {
     navigate("/product/pay", {
-      state: { cartList: cartList },
+      state: {
+        cartList: cartList,
+        totalPrice: totalCount.totalPrice,
+        member: member,
+      },
     });
   };
   // 천원단위 콤마붙인 가격
--- soolkkerbi_project_front/src/component/product/Pay.js
+++ soolkkerbi_project_front/src/component/product/Pay.js
@@ -1,5 +1,5 @@
 import "./pay.css";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Input from "../util/InputForm";
 import { useState } from "react";
 import { Button2 } from "../util/Buttons";
@@ -9,9 +9,12 @@ const Pay = (props) => {
   const location = useLocation();
   const cart = location.state.cart;
   const cartList = location.state.cartList;
-  const [memberName, setMemberName] = useState("");
-  const [memberPhone, setMemberPhone] = useState("");
-  const [memberEmail, setMemberEmail] = useState("");
+  const totalPrice = location.state.totalPrice;
+  const member = location.state.member;
+  const [memberName, setMemberName] = useState(member.memberName);
+  const [memberPhone, setMemberPhone] = useState(member.memberPhone);
+  const [memberEmail, setMemberEmail] = useState(member.memberEmail);
+  const [pickupDate, setPickupDate] = useState("");
   return (
     <div className="pay-all-wrap">
       <div className="pay-title">결제하기</div>
@@ -21,26 +24,26 @@ const Pay = (props) => {
           <div className="pay-member-info-wrap">
             <h3>주문자 정보</h3>
             <div className="pay-member-info">
+              <label htmlFor="memberName">이름</label>
               <Input
                 type="text"
                 data={memberName}
                 setData={setMemberName}
                 content="memberName"
-                placeholder="이름"
               />
+              <label htmlFor="memberPhone">연락처</label>
               <Input
                 type="text"
                 data={memberPhone}
                 setData={setMemberPhone}
                 content="memberPhone"
-                placeholder="연락처"
               />
+              <label htmlFor="memberEmail">이메일</label>
               <Input
                 type="text"
                 data={memberEmail}
                 setData={setMemberEmail}
                 content="memberEmail"
-                placeholder="이메일"
               />
             </div>
           </div>
@@ -56,8 +59,8 @@ const Pay = (props) => {
             <h3>방문 일자</h3>
             <Input
               type="text"
-              data={memberEmail}
-              setData={setMemberEmail}
+              data={pickupDate}
+              setData={setPickupDate}
               content="memberEmail"
               placeholder="EX) 2023-10-24 18:00"
             />
@@ -66,20 +69,7 @@ const Pay = (props) => {
         <div className="right-wrap">
           <div className="pay-summary-wrap">
             <h3>주문 요약</h3>
-            <div className="pay-summary">
-              <div className="product-price-wrap">
-                <span>상품가격</span>
-                <span>30100원</span>
-              </div>
-              <div className="delivery-price-wrap">
-                <span>배송비</span>
-                <span>방문수령(무료)</span>
-              </div>
-              <div className="total-price-wrap">
-                <span>총 주문금액</span>
-                <span>30100원</span>
-              </div>
-            </div>
+            <PaySummary cart={cart} totalPrice={totalPrice} />
           </div>
           <div className="pay-info-wrap">
             <h3>결제 안내</h3>
@@ -107,18 +97,26 @@ const Pay = (props) => {
 //장바구니에서 전체 상품 주문 시 -> cartList라는 배열로 받음
 const CartList = (props) => {
   const cartList = props.cartList;
+  const navigate = useNavigate();
+  const productView = (productNo) => {
+    navigate("/product/view", { state: { productNo: productNo } });
+  };
   return (
     <div className="pay-product-info-wrap">
       <h3>주문 상품 정보</h3>
       {cartList.map((cart, index) => {
-        console.log(cart.cartPrice);
         //천원 단위 콤마붙인 가격
         const commaPrice = cart.cartPrice
           .toString()
           .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         return (
           <div className="pay-product-info" key={"cart" + index}>
-            <div className="pay-product-img">
+            <div
+              className="pay-product-img"
+              onClick={() => {
+                productView(cart.cartProductNo);
+              }}
+            >
               <img src={"/product/" + cart.productFilepath} />
             </div>
             <div className="pay-product-info-detail">
@@ -135,7 +133,10 @@ const CartList = (props) => {
 //장바구니에서 개별 상품 주문 시 -> cart라는 객체로 받음
 const Cart = (props) => {
   const cart = props.cart;
-  console.log(cart.cartPrice);
+  const navigate = useNavigate();
+  const productView = () => {
+    navigate("/product/view", { state: { productNo: cart.cartProductNo } });
+  };
   //천원 단위 콤마붙인 가격
   const commaPrice = cart.cartPrice
     .toString()
@@ -144,7 +145,7 @@ const Cart = (props) => {
     <div className="pay-product-info-wrap">
       <h3>주문 상품 정보</h3>
       <div className="pay-product-info">
-        <div className="pay-product-img">
+        <div className="pay-product-img" onClick={productView}>
           <img src={"/product/" + cart.productFilepath} />
         </div>
         <div className="pay-product-info-detail">
@@ -152,6 +153,37 @@ const Cart = (props) => {
           <div className="product-detail-quantity">{cart.cartStock}개</div>
           <div className="product-detail-price">{commaPrice}원</div>
         </div>
+      </div>
+    </div>
+  );
+};
+//주문 요약
+const PaySummary = (props) => {
+  const cart = props.cart;
+  const totalPrice = props.totalPrice;
+  return (
+    <div className="pay-summary">
+      <div className="product-price-wrap">
+        <span>상품가격</span>
+        <span>
+          {totalPrice
+            ? totalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : cart.cartPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          원
+        </span>
+      </div>
+      <div className="delivery-price-wrap">
+        <span>배송비</span>
+        <span>방문수령(무료)</span>
+      </div>
+      <div className="total-price-wrap">
+        <span>총 주문금액</span>
+        <span>
+          {totalPrice
+            ? totalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : cart.cartPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          원
+        </span>
       </div>
     </div>
   );
