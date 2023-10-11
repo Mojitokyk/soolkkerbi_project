7d0fea2be4c714a73588988e79850398a7fc8ccb
gyeongeunpark <142005747+gyeongeunpark@users.noreply.github.com>
2023-10-11 18:20:58 +0900

    리뷰쓰기진행중

--- soolkkerbi_project_back/src/main/java/kr/or/skb/review/model/dao/ReviewDao.java
+++ soolkkerbi_project_back/src/main/java/kr/or/skb/review/model/dao/ReviewDao.java
@@ -2,7 +2,11 @@ package kr.or.skb.review.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.or.skb.review.model.vo.Review;
+
 @Mapper
 public interface ReviewDao {
+
+	int insertReview(Review r);
 	
 }
