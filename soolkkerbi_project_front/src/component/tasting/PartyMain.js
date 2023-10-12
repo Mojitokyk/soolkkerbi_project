1352a6b45b6ebbff32227ad520c4c50ef00465a0
DonghyoSon <143981349+DonghyoSon@users.noreply.github.com>
2023-10-12 14:53:35 +0900

    partyList.js getMember -> partyMain.js

--- soolkkerbi_project_front/src/component/tasting/PartyMain.js
+++ soolkkerbi_project_front/src/component/tasting/PartyMain.js
@@ -2,19 +2,48 @@ import { Routes, Route } from "react-router-dom";
 import PartyList from "./PartyList";
 import PartyView from "./PartyView";
 import PartyWrite from "./PartyWrite";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PartyMain = (props) => {
-    const isLogin = props.isLogin;
-    return (
-      <div className="party-all-wrap">
+  const isLogin = props.isLogin;
+
+  const [member, setMember] = useState({});
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (isLogin) {
+      axios
+        .post("/member/getMember", null, {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        })
+        .then((res) => {
+          setMember(res.data);
+        })
+        .catch((res) => {
+          console.log(res.response.status);
+        });
+    }
+  }, [isLogin]);
+
+  return (
+    <div className="party-all-wrap">
       <div className="party-title">시음회</div>
       <Routes>
         <Route path="write" element={<PartyWrite isLogin={isLogin} />}></Route>
-        <Route path="view" element={<PartyView isLogin={isLogin} />}></Route>
-        <Route path="*" element={<PartyList isLogin={isLogin} />}></Route>
+        <Route
+          path="view"
+          element={<PartyView isLogin={isLogin} member={member} />}
+        ></Route>
+        <Route
+          path="*"
+          element={<PartyList isLogin={isLogin} member={member} />}
+        ></Route>
       </Routes>
-      </div>
-    );
-  };
-  
-  export default PartyMain;
+    </div>
+  );
+};
+
+export default PartyMain;
